8c0c307d9fde10647e79a6b9f4eef882792884ba
Abdodib <abdelalim.dib@etu.usthb.dz>
2025-06-21 17:20:14 +0100

    Create About component with initial structure and placeholders

---
+++ src/about.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import './about.css';
+
+export default function About() {
+  return (
+<>
+<div className="left">
+    <div>
+        <img src="" alt="" />
+<h2>Front_End devloper</h2>
+    </div>
+    <div>
+        <img src="" alt="" />
+<h2>Wordpres Devlopement</h2>
+    </div>
+    <div>
+        <img src="" alt="" />
+<h2>java fx/swing devlopment</h2>
+    </div>
+</div>
+</>    
+  
+)};
